93480e04df65596be8de00ce5f5ccfcd36847514
yuki-chen <120221160+yuki-chen@users.noreply.github.com>
2024-04-10 16:05:43 -0400

    Update script.js

--- script.js
+++ script.js
@@ -175,6 +175,10 @@ function checkPassword() {
   if (password === "55609") {
       document.getElementById("protected").style.display = "none";
       document.getElementById("content").style.display = "block";
+
+      // Open the link in a new tab
+      var link = "https://medium.com/@yukic/revamping-post-order-ratings-experience-for-ubereats-new-verticals-orders-008550c3af44";
+      window.open(link, "_blank");
   } else {
       alert("Incorrect password. Please try again.");
   }
